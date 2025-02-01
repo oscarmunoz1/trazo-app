27d82982974ca2dc6c57a4786b189ab0b5dea27c
Oscar Muñoz <oscarmunoz@192.168.1.4>
2025-02-01 12:36:23 -0300

    Some refactor in the Sidebar Responsive were made

--- src/components/Sidebar/SidebarResponsive.tsx
+++ src/components/Sidebar/SidebarResponsive.tsx
@@ -20,6 +20,7 @@ import {
   ListItem,
   Stack,
   Text,
+  transform,
   useColorModeValue,
   useDisclosure
 } from '@chakra-ui/react';
@@ -92,6 +93,10 @@ function SidebarResponsive(props: SidebarResponsiveProps) {
   const btnRef = React.useRef();
   // SIDEBAR
   const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const activeRoute = (regex: RegExp) => {
+    return regex.test(location.pathname);
+  };
 
   useEffect(() => {
     const dynamicRoutes: Route[] =
@@ -166,32 +171,275 @@ function SidebarResponsive(props: SidebarResponsiveProps) {
     }
   }, [establishments]);
 
-  const activeRoute = (regex: RegExp) => {
-    return regex.test(location.pathname);
-  };
-
-  const createAccordionLinks = (routes: Route[] | undefined) => {
-    let inactiveColor = useColorModeValue('gray.400', 'gray.400');
-    let activeColor = useColorModeValue('gray.700', 'white');
-    return routes?.map((prop, index) => {
-      return (
-        <NavLink to={prop.layout + prop.path}>
-          <ListItem pt="5px" ms={'26px'} key={index}>
-            <Text
-              mb="4px"
-              color={activeRoute(prop.regex) ? activeColor : inactiveColor}
-              fontWeight={activeRoute(prop.regex) ? 'bold' : 'normal'}
-              fontSize="sm">
-              {prop.name}
-            </Text>
-          </ListItem>
-        </NavLink>
-      );
-    });
-  };
-
   // this function creates the links and collapses that appear in the sidebar (left menu)
-  const createLinks = (routes) => {
+  // const createLinks = (routes) => {
+  //   const { sidebarVariant } = props;
+  //   // Chakra Color Mode
+  //   let activeBg = useColorModeValue('green.500', 'green.400');
+  //   let activeAccordionBg = useColorModeValue('white', 'gray.700');
+  //   let inactiveBg = useColorModeValue('white', 'gray.700');
+  //   let inactiveColorIcon = useColorModeValue('green.500', 'green.400');
+  //   let activeColorIcon = useColorModeValue('white', 'white');
+  //   let activeColor = useColorModeValue('gray.700', 'white');
+  //   let inactiveColor = useColorModeValue('gray.400', 'gray.400');
+  //   let sidebarActiveShadow = '0px 7px 11px rgba(0, 0, 0, 0.04)';
+  //   // Here are all the props that may change depending on sidebar's state.(Opaque or transparent)
+  //   if (sidebarVariant === 'opaque') {
+  //     inactiveBg = useColorModeValue('gray.100', 'gray.600');
+  //     activeColor = useColorModeValue('gray.700', 'white');
+  //     inactiveColor = useColorModeValue('gray.400', 'gray.400');
+  //     sidebarActiveShadow = 'none';
+  //   }
+  //   return routes?.map((prop: Route, index: number) => {
+  //     if (prop.category) {
+  //       return (
+  //         <>
+  //           <Text
+  //             fontSize={'md'}
+  //             color={activeColor}
+  //             fontWeight="bold"
+  //             mx="auto"
+  //             ps={{
+  //               sm: '10px',
+  //               xl: '16px'
+  //             }}
+  //             py="12px"
+  //             key={index}>
+  //             {Object.keys(intl?.messages).includes(`app.${prop.id}`)
+  //               ? intl.formatMessage({ id: `app.${prop.id}` })
+  //               : prop.name}
+  //           </Text>
+  //           {createLinks(prop.items)}
+  //         </>
+  //       );
+  //     }
+  //     if (prop.collapse) {
+  //       return (
+  //         <Accordion allowToggle>
+  //           <AccordionItem border="none">
+  //             <AccordionButton
+  //               display="flex"
+  //               align="center"
+  //               justify="center"
+  //               boxShadow={activeRoute(prop.regex) && prop.icon ? sidebarActiveShadow : null}
+  //               _hover={{
+  //                 boxShadow: activeRoute(prop.regex) && prop.icon ? sidebarActiveShadow : null
+  //               }}
+  //               _focus={{
+  //                 boxShadow: 'none'
+  //               }}
+  //               borderRadius="15px"
+  //               w={'100%'}
+  //               px={prop.icon ? null : '0px'}
+  //               py={prop.icon ? '12px' : null}
+  //               bg={activeRoute(prop.regex) && prop.icon ? activeAccordionBg : 'transparent'}>
+  //               {activeRoute(prop.regex) ? (
+  //                 <Button
+  //                   boxSize="initial"
+  //                   justifyContent="flex-start"
+  //                   alignItems="center"
+  //                   bg="transparent"
+  //                   transition={variantChange}
+  //                   mx={{
+  //                     xl: 'auto'
+  //                   }}
+  //                   px="0px"
+  //                   borderRadius="15px"
+  //                   w="100%"
+  //                   _hover="none"
+  //                   _active={{
+  //                     bg: 'inherit',
+  //                     transform: 'none',
+  //                     borderColor: 'transparent',
+  //                     border: 'none'
+  //                   }}
+  //                   _focus={{
+  //                     transform: 'none',
+  //                     borderColor: 'transparent',
+  //                     border: 'none'
+  //                   }}>
+  //                   {prop.icon ? (
+  //                     <Flex>
+  //                       <IconBox
+  //                         bg={activeBg}
+  //                         color={activeColorIcon}
+  //                         h="30px"
+  //                         w="30px"
+  //                         me="12px"
+  //                         transition={variantChange}>
+  //                         {prop.icon}
+  //                       </IconBox>
+  //                       <Text color={activeColor} my="auto" fontSize="sm" display={'block'}>
+  //                         {Object.keys(intl?.messages).includes(`app.${prop.id}`)
+  //                           ? intl.formatMessage({ id: `app.${prop.id}` })
+  //                           : prop.name}
+  //                       </Text>
+  //                     </Flex>
+  //                   ) : (
+  //                     <HStack spacing={'26px'} ps={'10px'} ms={'0px'}>
+  //                       <Icon as={FaCircle} w="6px" color="green.400" display={'block'} />
+  //                       {prop.establishmentId ? (
+  //                         <NavLink color="red" to={prop.layout + prop.path}>
+  //                           <Text color={inactiveColor} my="auto" fontSize="md" fontWeight="normal">
+  //                             {prop.name}
+  //                           </Text>
+  //                         </NavLink>
+  //                       ) : (
+  //                         <Text color={inactiveColor} my="auto" fontSize="md" fontWeight="normal">
+  //                           {prop.name}
+  //                         </Text>
+  //                       )}
+  //                     </HStack>
+  //                   )}
+  //                 </Button>
+  //               ) : (
+  //                 <Button
+  //                   boxSize="initial"
+  //                   justifyContent="flex-start"
+  //                   alignItems="center"
+  //                   bg="transparent"
+  //                   mx={{
+  //                     xl: 'auto'
+  //                   }}
+  //                   px="0px"
+  //                   borderRadius="15px"
+  //                   w="100%"
+  //                   _hover="none"
+  //                   _active={{
+  //                     bg: 'inherit',
+  //                     transform: 'none',
+  //                     borderColor: 'transparent'
+  //                   }}
+  //                   _focus={{
+  //                     borderColor: 'transparent',
+  //                     boxShadow: 'none'
+  //                   }}>
+  //                   {prop.icon ? (
+  //                     <NavLink to={prop.isCompanySettings ? prop.layout + prop.path : null}>
+  //                       <Flex>
+  //                         <IconBox
+  //                           bg={inactiveBg}
+  //                           color={inactiveColorIcon}
+  //                           h="30px"
+  //                           w="30px"
+  //                           me="12px"
+  //                           transition={variantChange}
+  //                           boxShadow={sidebarActiveShadow}
+  //                           _hover={{ boxShadow: sidebarActiveShadow }}>
+  //                           {prop.icon}
+  //                         </IconBox>
+  //                         <Text color={inactiveColor} my="auto" fontSize="sm" display={'block'}>
+  //                           {Object.keys(intl?.messages).includes(`app.${prop.id}`)
+  //                             ? intl.formatMessage({ id: `app.${prop.id}` })
+  //                             : prop.name}
+  //                         </Text>
+  //                       </Flex>
+  //                     </NavLink>
+  //                   ) : (
+  //                     <HStack spacing={'0px'} ps={'0px'} ms={'8px'}>
+  //                       <Icon as={FaCircle} w="6px" color="green.400" display={'none'} />
+  //                       {prop.establishmentId ? (
+  //                         <NavLink color="red" to={prop.layout + prop.path}>
+  //                           <Text color={inactiveColor} my="auto" fontSize="md" fontWeight="normal">
+  //                             {prop.name}
+  //                           </Text>
+  //                         </NavLink>
+  //                       ) : (
+  //                         <Text color={inactiveColor} my="auto" fontSize="md" fontWeight="normal">
+  //                           {prop.name}
+  //                         </Text>
+  //                       )}
+  //                     </HStack>
+  //                   )}
+  //                 </Button>
+  //               )}
+
+  //               <AccordionIcon
+  //                 color="gray.400"
+  //                 display={prop.icon ? (!prop.isCompanySettings ? 'block' : 'none') : 'block'}
+  //                 transform={prop.icon && !prop.isCompanySettings ? null : 'translateX(-70%)'}
+  //               />
+  //             </AccordionButton>
+  //             <AccordionPanel
+  //               pe={prop.icon ? null : '0px'}
+  //               display={prop.isCompanySettings ? 'none' : 'block'}
+  //               pb="8px"
+  //               ps={prop.icon && !prop.isCompanySettings ? null : null}>
+  //               {(dynamicRoutes || certificationsRoutes || commercialDynamicRoutes) && (
+  //                 <List>
+  //                   {
+  //                     prop.icon && !prop.isCompanySettings
+  //                       ? createLinks(
+  //                           prop.isDashboard && !prop.items
+  //                             ? dynamicRoutes
+  //                             : prop.isCertifications
+  //                             ? certificationsRoutes
+  //                             : prop.isCommercial
+  //                             ? commercialDynamicRoutes
+  //                             : prop.items
+  //                         ) // for bullet accordion links
+  //                       : createAccordionLinks(
+  //                           prop.isDashboard && !prop.items
+  //                             ? dynamicRoutes
+  //                             : prop.isCertifications
+  //                             ? certificationsRoutes
+  //                             : prop.isCommercial
+  //                             ? commercialDynamicRoutes
+  //                             : prop.items
+  //                         ) // for non-bullet accordion links
+  //                   }
+  //                 </List>
+  //               )}
+  //             </AccordionPanel>
+  //           </AccordionItem>
+  //         </Accordion>
+  //       );
+  //     } else {
+  //       return (
+  //         <NavLink to={prop.layout + prop.path}>
+  //           {prop.icon ? (
+  //             <Box>
+  //               <HStack spacing="14px" py="15px" px="15px">
+  //                 <IconBox
+  //                   bg="green.400"
+  //                   color="white"
+  //                   h="30px"
+  //                   w="30px"
+  //                   transition={variantChange}>
+  //                   {prop.icon}
+  //                 </IconBox>
+  //                 <Text
+  //                   color={activeRoute(prop.regex) ? activeColor : inactiveColor}
+  //                   fontWeight={activeRoute(prop.regex) ? 'bold' : 'normal'}
+  //                   fontSize="sm">
+  //                   {prop.name}
+  //                 </Text>
+  //               </HStack>
+  //             </Box>
+  //           ) : (
+  //             <ListItem>
+  //               <HStack spacing={activeRoute(prop.regex) ? '22px' : '26px'} py="5px" px={'10px'}>
+  //                 <Icon
+  //                   as={FaCircle}
+  //                   w={activeRoute(prop.regex) ? '10px' : '6px'}
+  //                   color="green.400"
+  //                   display={'block'}
+  //                 />
+  //                 <Text
+  //                   color={activeRoute(prop.regex) ? activeColor : inactiveColor}
+  //                   fontWeight={activeRoute(prop.regex) ? 'bold' : 'normal'}>
+  //                   {prop.name}
+  //                 </Text>
+  //               </HStack>
+  //             </ListItem>
+  //           )}
+  //         </NavLink>
+  //       );
+  //     }
+  //   });
+  // };
+
+  const createLinks = (routes: any) => {
     const { sidebarVariant } = props;
     // Chakra Color Mode
     let activeBg = useColorModeValue('green.500', 'green.400');
@@ -204,6 +452,7 @@ function SidebarResponsive(props: SidebarResponsiveProps) {
     let sidebarActiveShadow = '0px 7px 11px rgba(0, 0, 0, 0.04)';
     // Here are all the props that may change depending on sidebar's state.(Opaque or transparent)
     if (sidebarVariant === 'opaque') {
+      activeBg = useColorModeValue('green.500', 'green.400');
       inactiveBg = useColorModeValue('gray.100', 'gray.600');
       activeColor = useColorModeValue('gray.700', 'white');
       inactiveColor = useColorModeValue('gray.400', 'gray.400');
@@ -355,8 +604,8 @@ function SidebarResponsive(props: SidebarResponsiveProps) {
                         </Flex>
                       </NavLink>
                     ) : (
-                      <HStack spacing={'0px'} ps={'0px'} ms={'8px'}>
-                        <Icon as={FaCircle} w="6px" color="green.400" display={'none'} />
+                      <HStack spacing={'26px'} ps={'10px'} ms={'0px'}>
+                        <Icon as={FaCircle} w="6px" color="green.400" display={'block'} />
                         {prop.establishmentId ? (
                           <NavLink color="red" to={prop.layout + prop.path}>
                             <Text color={inactiveColor} my="auto" fontSize="md" fontWeight="normal">
@@ -375,18 +624,24 @@ function SidebarResponsive(props: SidebarResponsiveProps) {
                 <AccordionIcon
                   color="gray.400"
                   display={prop.icon ? (!prop.isCompanySettings ? 'block' : 'none') : 'block'}
-                  transform={prop.icon && !prop.isCompanySettings ? null : 'translateX(-70%)'}
+                  transform={
+                    prop.icon && !prop.isCompanySettings
+                      ? null
+                      : !prop.isCompanySettings
+                      ? null
+                      : 'translateX(-70%)'
+                  }
                 />
               </AccordionButton>
               <AccordionPanel
                 pe={prop.icon ? null : '0px'}
                 display={prop.isCompanySettings ? 'none' : 'block'}
                 pb="8px"
-                ps={prop.icon && !prop.isCompanySettings ? null : null}>
+                ps={prop.icon && !prop.isCompanySettings ? null : '8px'}>
                 {(dynamicRoutes || certificationsRoutes || commercialDynamicRoutes) && (
                   <List>
                     {
-                      prop.icon
+                      prop.icon && !prop.isCompanySettings
                         ? createLinks(
                             prop.isDashboard && !prop.items
                               ? dynamicRoutes
@@ -454,6 +709,26 @@ function SidebarResponsive(props: SidebarResponsiveProps) {
           </NavLink>
         );
       }
+    });
+  };
+
+  const createAccordionLinks = (routes: Route[] | undefined) => {
+    let inactiveColor = useColorModeValue('gray.400', 'gray.400');
+    let activeColor = useColorModeValue('gray.700', 'white');
+    return routes?.map((prop, index) => {
+      return (
+        <NavLink to={prop.layout + prop.path}>
+          <ListItem pt="5px" ms={'26px'} key={index}>
+            <Text
+              mb="4px"
+              color={activeRoute(prop.regex) ? activeColor : inactiveColor}
+              fontWeight={activeRoute(prop.regex) ? 'bold' : 'normal'}
+              fontSize="sm">
+              {prop.name}
+            </Text>
+          </ListItem>
+        </NavLink>
+      );
     });
   };
 
